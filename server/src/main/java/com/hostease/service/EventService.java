19fa702729a8bc73d264c119a2ef5877157b5d9e
Enrique <enriquedlc2d@gmail.com>
2023-04-21 12:27:42 +0100

    Feature: created Event service.

---
+++ server/src/main/java/com/hostease/service/EventService.java
@@ -0,0 +1,33 @@
+package com.hostease.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hostease.entity.Event;
+import com.hostease.repository.EventRepository;
+
+@Service
+public class EventService {
+
+    @Autowired
+    EventRepository eventRepository;
+
+    public List<Event> findAll() {
+        return eventRepository.findAll();
+    }
+
+    public Event findById(Long id) {
+        return eventRepository.findById(id).get();
+    }
+
+    public Event save(Event event) {
+        return eventRepository.save(event);
+    }
+
+    public void deleteById(Long id) {
+        eventRepository.deleteById(id);
+    }
+
+}
