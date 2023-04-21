2f8a0e6aa2f9aded9cb4a5942fc74a6a2ac13e4b
Enrique <enriquedlc2d@gmail.com>
2023-04-21 12:29:09 +0100

    Feature: added GET endpoint.
    1. localhost:8080/hostease/events retrieves all events.

---
+++ server/src/main/java/com/hostease/controller/EventController.java
@@ -0,0 +1,46 @@
+package com.hostease.controller;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hostease.entity.Event;
+import com.hostease.service.EventService;
+
+@RestController
+@RequestMapping("/hostease")
+public class EventController {
+
+    @Autowired
+    EventService eventService;
+
+    @GetMapping("/events")
+    public ResponseEntity<Map<String, Object>> findAll() {
+        Map<String, Object> response = new LinkedHashMap<String, Object>();
+
+        try {
+            List<Event> events = eventService.findAll();
+
+            response.put("status", "200 OK");
+            response.put("message", "Events successfully retrieved");
+            response.put("data", events);
+
+            return ResponseEntity.status(200).body(response);
+        } catch (Exception e) {
+            response.put("status", "500 Internal Server Error");
+            response.put("message", "Error retrieving events");
+            response.put("error", e.getMessage());
+            response.put("data", null);
+
+            return ResponseEntity.status(500).body(response);
+        }
+
+    }
+
+}
