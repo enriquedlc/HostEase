fca901f0367acac51d4a6a6f4c98ffada35d281d
Enrique <enriquedlc2d@gmail.com>
2023-04-24 15:49:46 +0100

    Feature: added GET endpoint for events.
    GET -> localhost:8080/hostease/events/{id}.
    
    Retrieves the event specified by id.

--- server/src/main/java/com/hostease/controller/EventController.java
+++ server/src/main/java/com/hostease/controller/EventController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,25 +24,49 @@ public class EventController {
 
     @GetMapping("/events")
     public ResponseEntity<Map<String, Object>> findAll() {
-        Map<String, Object> response = new LinkedHashMap<String, Object>();
+        Map<String, Object> jsonResponseMap = new LinkedHashMap<String, Object>();
 
         try {
             List<Event> events = eventService.findAll();
 
-            response.put("status", HttpStatusEnum.STATUS_200_OK.getStatus());
-            response.put("message", "Events successfully retrieved");
-            response.put("data", events);
+            jsonResponseMap.put("status", HttpStatusEnum.STATUS_200_OK.getStatus());
+            jsonResponseMap.put("message", "Events successfully retrieved");
+            jsonResponseMap.put("data", events);
 
-            return ResponseEntity.status(200).body(response);
+            return ResponseEntity.status(200).body(jsonResponseMap);
         } catch (Exception e) {
-            response.put("status", HttpStatusEnum.STATUS_500_INTERNAL_SERVER_ERROR.getStatus());
-            response.put("message", "Error retrieving events");
-            response.put("error", e.getMessage());
-            response.put("data", null);
+            jsonResponseMap.put("status", HttpStatusEnum.STATUS_500_INTERNAL_SERVER_ERROR.getStatus());
+            jsonResponseMap.put("message", "Error retrieving events");
+            jsonResponseMap.put("error", e.getMessage());
+            jsonResponseMap.put("data", null);
 
-            return ResponseEntity.status(500).body(response);
+            return ResponseEntity.status(500).body(jsonResponseMap);
         }
 
     }
+
+    @GetMapping("/events/{id}")
+    public ResponseEntity<Map<String, Object>> findById(@PathVariable("id") Long id) {
+        Map<String, Object> jsonResponseMap = new LinkedHashMap<String, Object>();
+
+        try {
+            Event event = eventService.findById(id);
+
+            jsonResponseMap.put("status", HttpStatusEnum.STATUS_200_OK.getStatus());
+            jsonResponseMap.put("message", "Event successfully retrieved");
+            jsonResponseMap.put("data", event);
+
+            return ResponseEntity.status(200).body(jsonResponseMap);
+        } catch (Exception e) {
+            jsonResponseMap.put("status", HttpStatusEnum.STATUS_500_INTERNAL_SERVER_ERROR.getStatus());
+            jsonResponseMap.put("message", "Error retrieving event");
+            jsonResponseMap.put("error", e.getMessage());
+            jsonResponseMap.put("data", null);
+
+            return ResponseEntity.status(500).body(jsonResponseMap);
+        }
+
+    }
+
 
 }
