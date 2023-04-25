bd37d52c0a71751042b0dada5dd290e45f149b5c
Enrique <enriquedlc2d@gmail.com>
2023-04-25 12:30:11 +0100

    Feature: POST method for event.
    POST localhost:8080/hostease/events?categoryId=1
    
    Saves an event, using the {categoryId} as a parameter
    to specify the category that the event belongs.

--- server/src/main/java/com/hostease/controller/EventController.java
+++ server/src/main/java/com/hostease/controller/EventController.java
@@ -8,11 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hostease.entity.Category;
 import com.hostease.entity.Event;
 import com.hostease.enums.HttpStatusEnum;
+import com.hostease.repository.CategoryRepository;
 import com.hostease.service.EventService;
 
 @RestController
@@ -21,6 +26,9 @@ public class EventController {
 
     @Autowired
     EventService eventService;
+
+    @Autowired
+    CategoryRepository categoryRepository;
 
     @GetMapping("/events")
     public ResponseEntity<Map<String, Object>> findAll() {
@@ -68,5 +76,29 @@ public class EventController {
 
     }
 
+    @PostMapping("/events{categoryId}")
+    public ResponseEntity<Map<String, Object>> save(@RequestBody Event event, @RequestParam("categoryId") Long id) {
+        Map<String, Object> jsonResponseMap = new LinkedHashMap<String, Object>();
+
+        try {
+            Category categoryToAssign = categoryRepository.findById(id).get();
+            categoryRepository.save(categoryToAssign);
+            Event savedEvent = eventService.save(event, id);
+
+            jsonResponseMap.put("status", HttpStatusEnum.STATUS_200_OK.getStatus());
+            jsonResponseMap.put("message", "Event successfully saved");
+            jsonResponseMap.put("data", savedEvent);
+
+            return ResponseEntity.status(200).body(jsonResponseMap);
+        } catch (Exception e) {
+            jsonResponseMap.put("status", HttpStatusEnum.STATUS_500_INTERNAL_SERVER_ERROR.getStatus());
+            jsonResponseMap.put("message", "Error saving event");
+            jsonResponseMap.put("error", e.getMessage());
+            jsonResponseMap.put("data", null);
+
+            return ResponseEntity.status(500).body(jsonResponseMap);
+        }
+
+    }
 
 }
