c251d1f07b17e43663a12568d4f9aceb93f60059
Enrique <enriquedlc2d@gmail.com>
2023-04-28 14:13:39 +0100

    Refactor(TagController): used the jsonResponseMapGenerator method.

--- server/src/main/java/com/hostease/controller/TagController.java
+++ server/src/main/java/com/hostease/controller/TagController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.hostease.entity.Tag;
 import com.hostease.enums.HttpStatusEnum;
 import com.hostease.service.TagService;
+import com.hostease.utils.ControllerJsonResponseMap;
 
 @RestController
 @RequestMapping("/hostease")
@@ -26,99 +27,51 @@ public class TagController {
     TagService tagService;
 
     @GetMapping("/tags")
-    public ResponseEntity<?> findAll() {
-        Map<String, Object> response = new LinkedHashMap<String, Object>();
+    public ResponseEntity<Map<String, Object>> findAll() {
 
-        try {
-            List<Tag> tags = tagService.findAll();
+        List<Tag> tags = tagService.findAll();
 
-            response.put("status", HttpStatusEnum.STATUS_200_OK.getStatus());
-            response.put("message", "Tags successfully retrieved");
-            response.put("data", tags);
-
-            return ResponseEntity.status(200).body(response);
-
-        } catch (Exception e) {
-            response.put("status", HttpStatusEnum.STATUS_500_INTERNAL_SERVER_ERROR.getStatus());
-            response.put("message", "Error retrieving tags");
-            response.put("error", e.getMessage());
-            response.put("data", null);
-
-            return ResponseEntity.status(500).body(response);
-        }
-
+        return new ControllerJsonResponseMap().jsonResponseMapListGenerator(
+                tags,
+                HttpStatusEnum.STATUS_200_OK.getStatus(),
+                "Tags successfully retrieved",
+                "Error retrieving tags");
     }
 
     @GetMapping("/tags/{id}")
     public ResponseEntity<?> findById(@PathVariable("id") Long id) {
-        Map<String, Object> response = new LinkedHashMap<String, Object>();
 
-        try {
-            Tag tag = tagService.findById(id);
+        Tag tag = tagService.findById(id);
 
-            response.put("status", HttpStatusEnum.STATUS_200_OK.getStatus());
-            response.put("message", "Tag successfully retrieved");
-            response.put("data", tag);
-
-            return ResponseEntity.status(200).body(response);
-
-        } catch (Exception e) {
-            response.put("status", HttpStatusEnum.STATUS_500_INTERNAL_SERVER_ERROR.getStatus());
-            response.put("message", "Error retrieving tag");
-            response.put("error", e.getMessage());
-            response.put("data", null);
-
-            return ResponseEntity.status(500).body(response);
-        }
-
+        return new ControllerJsonResponseMap().jsonResponseMapObjectGenerator(
+                tag,
+                HttpStatusEnum.STATUS_200_OK.getStatus(),
+                "Tag successfully retrieved",
+                "Error retrieving tag");
     }
 
     @PostMapping("/tags")
-    public ResponseEntity<?> save(@RequestBody Tag tag) {
-        Map<String, Object> response = new LinkedHashMap<String, Object>();
+    public ResponseEntity<Map<String, Object>> save(@RequestBody Tag tag) {
 
-        try {
-            Tag tagToSave = tagService.save(tag);
+        Tag tagToSave = tagService.save(tag);
 
-            response.put("status", HttpStatusEnum.STATUS_201_CREATED.getStatus());
-            response.put("message", "Tag successfully saved");
-            response.put("data", tagToSave);
-
-            return ResponseEntity.status(200).body(response);
-
-        } catch (Exception e) {
-            response.put("status", HttpStatusEnum.STATUS_500_INTERNAL_SERVER_ERROR.getStatus());
-            response.put("message", "Error saving tag");
-            response.put("error", e.getMessage());
-            response.put("data", null);
-
-            return ResponseEntity.status(500).body(response);
-        }
-
+        return new ControllerJsonResponseMap().jsonResponseMapObjectGenerator(
+                tagToSave,
+                HttpStatusEnum.STATUS_201_CREATED.getStatus(),
+                "Tag successfully created",
+                "Error creating tag");
     }
 
     @DeleteMapping("/tags/{id}")
     public ResponseEntity<?> deleteById(@PathVariable("id") Long id) {
-        Map<String, Object> response = new LinkedHashMap<String, Object>();
 
-        try {
-            Tag tag = tagService.findById(id);
-            tagService.deleteById(id);
+        tagService.deleteById(id);
 
-            response.put("status", HttpStatusEnum.STATUS_200_OK.getStatus());
-            response.put("message", String.format("Tag %s successfully deleted", tag.getTag()));
+        Map<String, Object> responseMap = new LinkedHashMap<>();
+        responseMap.put("status", HttpStatusEnum.STATUS_200_OK.getStatus());
+        responseMap.put("message", "Tag successfully deleted");
 
-            return ResponseEntity.status(200).body(response);
-
-        } catch (Exception e) {
-            response.put("status", HttpStatusEnum.STATUS_500_INTERNAL_SERVER_ERROR.getStatus());
-            response.put("message", "Error deleting tag");
-            response.put("error", e.getMessage());
-            response.put("data", null);
-
-            return ResponseEntity.status(500).body(response);
-        }
-
+        return ResponseEntity.ok().body(responseMap);
     }
 
 }
