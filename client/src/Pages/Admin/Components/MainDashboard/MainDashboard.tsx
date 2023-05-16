ed4c39be26312f9d2bfce4ffcfcf55d4643ef4f0
Enrique <enriquedlc2d@gmail.com>
2023-05-16 12:26:01 +0100

    Feature: created MainDashboard component.
    this component will display importat information to the admin.

---
+++ client/src/Pages/Admin/Components/MainDashboard/MainDashboard.tsx
@@ -0,0 +1,14 @@
+import Cards from '../Cards/Cards'
+import './MainDashboard.css'
+
+
+const MainDashboard = () => {
+  return (
+    <div className="main-dashboard">
+        <h1>Dashboard</h1>
+        <Cards/>
+    </div>
+  )
+}
+
+export default MainDashboard
