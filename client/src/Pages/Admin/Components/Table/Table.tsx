be1f909300f758f791b26950495c963c6950d086
Enrique <enriquedlc2d@gmail.com>
2023-05-19 20:10:34 +0100

    Feature:sparkles:: new styles and field for the table.

--- client/src/Pages/Admin/Components/Table/Table.tsx
+++ client/src/Pages/Admin/Components/Table/Table.tsx
@@ -7,19 +7,19 @@ const columns: GridColDef[] = [
     {
         field: 'title',
         headerName: 'Title',
-        width: 180
+        width: 250
     },
     {
         field: 'startDate',
         headerName: 'Start date',
-        width: 160,
+        width: 180,
         editable: false,
         align: 'left',
     },
     {
         field: 'startTime',
         headerName: 'Start time',
-        width: 120,
+        width: 180,
         editable: false,
         align: 'left',
     },
@@ -27,26 +27,34 @@ const columns: GridColDef[] = [
         field: 'maxCapacity',
         headerName: 'Capacity',
         type: 'number',
-        width: 120,
+        width: 180,
         editable: false,
-        align: 'center',
+        align: 'left',
     },
+    {
+        field: 'users',
+        headerName: 'Joined users',
+        type: 'number',
+        width: 180,
+        editable: false,
+        align: 'left',
+    }
 ];
 
 const rows = [
-    { id: 1, title: 'Car Event', startDate: '10/10/2021', startTime: '10:00', maxCapacity: 10 },
-    { id: 2, title: 'Tech Event', startDate: '10/10/2021', startTime: '10:00', maxCapacity: 10 },
-    { id: 3, title: 'Music Event', startDate: '10/10/2021', startTime: '10:00', maxCapacity: 10 },
-    { id: 4, title: 'Food Event', startDate: '10/10/2021', startTime: '10:00', maxCapacity: 10 },
-    { id: 5, title: 'Meet and Greet', startDate: '10/10/2021', startTime: '10:00', maxCapacity: 10 },
-    { id: 6, title: 'Festival', startDate: '10/10/2021', startTime: '10:00', maxCapacity: 10 },
+    { id: 1, title: 'Car Event', startDate: '10/10/2021', startTime: '10:00', maxCapacity: 10, users: 5 },
+    { id: 2, title: 'Tech Event', startDate: '10/10/2021', startTime: '10:00', maxCapacity: 10, users: 5 },
+    { id: 3, title: 'Music Event', startDate: '10/10/2021', startTime: '10:00', maxCapacity: 10, users: 5 },
+    { id: 4, title: 'Food Event', startDate: '10/10/2021', startTime: '10:00', maxCapacity: 10, users: 5 },
+    { id: 5, title: 'Meet and Greet', startDate: '10/10/2021', startTime: '10:00', maxCapacity: 10, users: 5 },
+    { id: 6, title: 'Festival', startDate: '10/10/2021', startTime: '10:00', maxCapacity: 10, users: 5 },
 ]
 
 const DataTable = () => {
     return (
         <div className='table'>
             <h3 className='table-title'>Recent Events</h3>
-            <Box sx={{ height: 400, width: '100%', background: 'white', maxHeight: '70%' }}>
+            <Box sx={{ height: 520, width: '100%', background: 'white', maxHeight: '70%' }}>
                 <DataGrid
                     rows={rows}
                     columns={columns}
