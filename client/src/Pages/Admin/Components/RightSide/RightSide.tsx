c6875df8c24e0b56943f46e1483e1ca2e31cb7de
Enrique <enriquedlc2d@gmail.com>
2023-05-25 10:21:52 +0100

    Feature: conditional routing render.

--- client/src/Pages/Admin/Components/RightSide/RightSide.tsx
+++ client/src/Pages/Admin/Components/RightSide/RightSide.tsx
@@ -1,22 +1,38 @@
+import { useLocation } from 'react-router-dom';
 import { User } from '../../../../Types/Types';
 import CustomContent from '../CustomContent/CustomContent';
 import Updates from '../Updates/Updates';
 
 import './RightSide.css';
 
+
 const RightSide = ({ users }: { users: User[] }) => {
+    const location = useLocation();
+
+    const isOnAdminRoute = location.pathname === '/admin';
+
     return (
         <div className="right-side">
-            <div>
-                <h3>Updates</h3>
-                <Updates users={users} />
-            </div>
-            <div className='custom-content-chart'>
-                <h3>Custom content</h3>
-                <CustomContent />
-            </div>
+            {isOnAdminRoute ? (
+                <>
+                    <div>
+                        <h3>Updates</h3>
+                        <Updates users={users} />
+                    </div>
+                    <div className='custom-content-chart'>
+                        <h3>Custom content</h3>
+                        <CustomContent />
+                    </div>
+                </>
+            ) : (
+                <div>
+                    <h3>Updates</h3>
+                    <Updates users={users} />
+                </div>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default RightSide;
+
