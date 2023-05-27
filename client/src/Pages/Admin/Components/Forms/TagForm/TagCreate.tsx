304dbcaeae0da2a5d779e7dc3df981431ef1fe1c
Enrique <enriquedlc2d@gmail.com>
2023-05-27 18:32:35 +0100

    Feature: create tag form.

--- client/src/Pages/Admin/Components/Forms/TagForm/TagCreate.tsx
+++ client/src/Pages/Admin/Components/Forms/TagForm/TagCreate.tsx
@@ -1,84 +1,98 @@
-import React from "react";
-import { Formik, Form, Field, FormikValues } from "formik";
+import { Form, Formik } from "formik";
 import { AnimatePresence, motion } from "framer-motion";
-import { addTag } from "../../../../../services/main.services";
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import { HostEaseRoutes } from "../../../../../Types/AppRoutes/HostEaseRoutes";
 import { ITag } from "../../../../../Types/Types";
+import { addTag } from "../../../../../services/main.services";
+
 import "./TagForm.css";
 
-type Props = {};
-
 const tagToCreate: ITag = {
-  tag: "",
-  color: "",
+    tag: "",
+    color: "",
 };
 
-const TagCreate: React.FC<Props> = (props: Props) => {
-  const createTag = async (values: FormikValues, { resetForm }: any) => {
-    if (!values.tag || !values.color) {
-      alert("Please enter a tag and color");
-      return;
-    }
+const TagCreate: React.FC = () => {
 
-    tagToCreate.tag = values.tag;
-    tagToCreate.color = values.color;
+    const [tag, setTag] = React.useState<ITag>(tagToCreate);
+    const navigate = useNavigate()
 
-    console.log(tagToCreate);
+    const createTag = async () => {
+        if (tag && !tag.tag || !tag.color || tag.tag === "" || tag.color === "") {
+            toast.error("Please fill all the fields.", {
+                position: "top-right",
+            });
+        } else {
+            await addTag(tag);
+            toast.success("Tag created successfully");
+            setTimeout(() => {
+                navigate(HostEaseRoutes.AdminTags);
+            }, 2000);
+        }
+    };
 
-    await addTag(tagToCreate);
+    console.log(tag)
 
-    resetForm(); // Reset the form after successful submission
-  };
+    const handleChangeText = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTag({
+            ...tag,
+            [e.target.name]: e.target.value,
+        })
+    };
 
-  const fieldAnimation = {
-    initial: { opacity: 0, y: -20 },
-    animate: { opacity: 1, y: 0 },
-    exit: { opacity: 0, y: -20 },
-  };
 
-  const buttonAnimation = {
-    initial: { opacity: 0, scale: 0.8 },
-    animate: { opacity: 1, scale: 1 },
-    exit: { opacity: 0, scale: 0.8 },
-    whileHover: { scale: 1.1 },
-    whileTap: { scale: 0.9 },
-  };
+    const fieldAnimation = {
+        initial: { opacity: 0, y: -20 },
+        animate: { opacity: 1, y: 0 },
+        exit: { opacity: 0, y: -20 },
+    };
 
-  return (
-    <div className="form-container">
-      <AnimatePresence>
-        <Formik
-          onSubmit={createTag}
-          initialValues={{
-            tag: "",
-            color: "",
-          }}
-        >
-          {({ handleSubmit }) => (
-            <Form className="form" onSubmit={handleSubmit}>
-              <motion.h3 {...fieldAnimation} className="form-title">
-                Create tag
-              </motion.h3>
-              <motion.div {...fieldAnimation} className="form-field">
-                <label htmlFor="tag">Tag:</label>
-                <Field className="field" id="tag" name="tag" type="text" />
-              </motion.div>
-              <motion.div {...fieldAnimation} className="form-field">
-                <label htmlFor="color">Color:</label>
-                <Field className="field" id="color" name="color" type="text" />
-              </motion.div>
-              <motion.button
-                {...buttonAnimation}
-                className="submit-button"
-                type="submit"
-              >
-                Create
-              </motion.button>
-            </Form>
-          )}
-        </Formik>
-      </AnimatePresence>
-    </div>
-  );
+    const buttonAnimation = {
+        initial: { opacity: 0, scale: 0.8 },
+        animate: { opacity: 1, scale: 1 },
+        exit: { opacity: 0, scale: 0.8 },
+        whileHover: { scale: 1.1 },
+        whileTap: { scale: 0.9 },
+    };
+
+    return (
+        <div className="form-container">
+            <AnimatePresence>
+                <ToastContainer />
+                <Formik
+                    onSubmit={createTag}
+                    initialValues={{
+                        tag: "",
+                        color: "",
+                    }}>
+                    {({ handleSubmit }) => (
+                        <Form className="form" onSubmit={handleSubmit}>
+                            <motion.h3 {...fieldAnimation} className="form-title">
+                                Create tag
+                            </motion.h3>
+                            <motion.div {...fieldAnimation} className="form-field">
+                                <label htmlFor="tag">Tag:</label>
+                                <input onChange={handleChangeText} className="field" id="tag" name="tag" type="text" />
+                            </motion.div>
+                            <motion.div {...fieldAnimation} className="form-field">
+                                <label htmlFor="color">Color:</label>
+                                <input onChange={handleChangeText} className="field" id="color" name="color" type="text" />
+                            </motion.div>
+                            <motion.button
+                                {...buttonAnimation}
+                                className="submit-button"
+                                type="submit"
+                            >
+                                Create
+                            </motion.button>
+                        </Form>
+                    )}
+                </Formik>
+            </AnimatePresence>
+        </div>
+    );
 };
 
 export default TagCreate;
