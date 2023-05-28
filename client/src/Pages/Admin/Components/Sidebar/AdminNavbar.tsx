8a55cb44e033592bb4769fe820e53fca03aeaba7
Enrique <enriquedlc2d@gmail.com>
2023-05-28 20:31:55 +0100

    changed file names and separed some objects.

--- client/src/Pages/Admin/Components/Sidebar/AdminNavbar.tsx
+++ client/src/Pages/Admin/Components/Sidebar/AdminNavbar.tsx
@@ -1,51 +1,12 @@
 import { motion } from 'framer-motion'
 import { useState } from 'react'
-import { MdLogout, MdMenu, MdOutlineCategory, MdOutlineComment, MdOutlineEditCalendar, MdOutlineGroup, MdOutlineHouse, MdTag } from 'react-icons/md'
+import { MdLogout, MdMenu } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 
-import { HostEaseRoutes } from '../../../../Types/AppRoutes/HostEaseRoutes'
 import HostEaseLogo from '../../../../assets/HostEase.png'
 
 import './AdminNavbar.css'
-
-type SidebarOptions = {
-    icon: any
-    title: string
-    route?: string
-}
-
-const SidebarData: SidebarOptions[] = [
-    {
-        icon: MdOutlineHouse,
-        title: "Dashboard",
-        route: HostEaseRoutes.Admin
-    },
-    {
-        icon: MdOutlineEditCalendar,
-        title: "Events",
-        route: HostEaseRoutes.AdminEvents
-    },
-    {
-        icon: MdOutlineGroup,
-        title: "Users",
-        route: HostEaseRoutes.AdminUsers
-    },
-    {
-        icon: MdTag,
-        title: "Tags",
-        route: HostEaseRoutes.AdminTags
-    },
-    {
-        icon: MdOutlineCategory,
-        title: "Categories",
-        route: HostEaseRoutes.AdminCategories
-    },
-    {
-        icon: MdOutlineComment,
-        title: "Comments",
-        route: HostEaseRoutes.AdminComments
-    }
-]
+import { AdminNavbarOptions } from './AdminNavbarOptions'
 
 const sidebarVariants = {
     true: {
@@ -61,7 +22,7 @@ const AdminNavbar: React.FC = () => {
     const [selected, setSelected] = useState<number>(0)
     const [showMenu, setShowMenu] = useState<boolean>(false)
 
-    const getRoutePath = (title: string): string => SidebarData.find(item => item.title === title)?.route ?? '';
+    const getRoutePath = (title: string): string => AdminNavbarOptions.find(item => item.title === title)?.route ?? '';
 
     const handleShowMenu = () => {
         setShowMenu(!showMenu)
@@ -90,7 +51,7 @@ const AdminNavbar: React.FC = () => {
                 </div>
                 {/* menu */}
                 <div className="menu">
-                    {SidebarData.map((item, index) => {
+                    {AdminNavbarOptions.map((item, index) => {
                         return (
                             <Link
                                 to={getRoutePath(item.title)}
